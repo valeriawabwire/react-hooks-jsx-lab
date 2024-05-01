1e437405126d9a2929ad103b8fb36ca46b638104
valeria wabwire <larriebliss@gmail.com>
2024-05-01 09:31:13 +0300

    first comit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
 import { name, city } from "../data/data.js";
+import { image } from "../data/data.js"; // Importing image variable from data.js
+import { render, screen } from '@testing-library/react';
+import Home from './Home'; // Import the Home component
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>Your {name} is a Web Developer from Your {city}</h1>
+      <img src={image} alt="I made this" /> {/* Use the img tag instead of image */}
+      test('the h1 has a an inline style attribute with a color of firebrick', () => {
+  render(<Home />);
+  const h1 = screen.getByText(/is a Web Developer from/i);
+  
+  expect(h1).toHaveStyle('color: firebrick');
+});
+   
+    </div>
+  );
 }
 
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,25 @@
+// import React from "react";
+
+// function NavBar() {
+//   // update the JSX being returned!
+//   return <nav>
+// <a href="#home">Home</a>
+// <a href="#about">About</a>
+
+//   </nav>;
+// }
+
+// export default NavBar;
 import React from "react";
 
 function NavBar() {
-  // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+    <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+    </nav>
+  );
 }
-
 export default NavBar;
